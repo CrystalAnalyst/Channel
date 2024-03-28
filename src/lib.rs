b2d7e91a37703a7749875d0b597283139adde2c6
zkthernum <kevin91cooland@gmail.com>
2024-03-28 23:14:09 +0800

    add AtomicOption and Seat

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #![allow(unused)]
 #![allow(dead_code)]
 
-use std::cell::UnsafeCell;
+use std::{
+    cell::UnsafeCell,
+    marker::PhantomData,
+    sync::{atomic, Arc},
+    thread,
+};
 
 /// Represents the state of a Seat in the circular buffer.
 struct SeatState<T> {
@@ -11,3 +16,14 @@ struct SeatState<T> {
 
 struct MutSeatState<T>(UnsafeCell<SeatState<T>>);
 
+struct AtomicOption<T> {
+    ptr: atomic::AtomicPtr<T>,
+    _marker: PhantomData<Option<Box<T>>>,
+}
+
+/// A seat represents a single location in the circurlar buffer.
+struct Seat<T> {
+    read: atomic::AtomicUsize,
+    state: MutSeatState<T>,
+    waiting: AtomicOption<thread::Thread>,
+}
